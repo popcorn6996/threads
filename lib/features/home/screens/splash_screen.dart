b225728d7eb8e356919d41332019a57aa0c80a7a
Popcorn <ediyiechris@gmail.com>
2023-07-10 20:53:50 +0100

    Uploading code

--- lib/features/home/screens/splash_screen.dart
+++ lib/features/home/screens/splash_screen.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:th/features/auth/screens/log_in_screen.dart';
-import 'package:th/features/home/screens/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
